d2ca2f25d11ad8fba3916770a85219afe154c877
klolik <klolik@ya.ru>
2017-07-21 01:23:04 +0300

    .

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@
 #include <string.h>
 
 
+// local
+
 
 
 using std::array;
@@ -31,88 +33,92 @@ using std::ios_base;
 
 
 // Побитовая структура для хранения состояние клетки
-struct point {
+struct Cell {
 	bool is_live = false;
 
-  bool operator== (const point& x) const {
-    return is_live == x.is_live;
-  }
+	bool operator== (const Cell& x) const {
+		return is_live == x.is_live;
+	}
 };
 
-std::ostream& operator<< (std::ostream& os, const point& p) {
-  return os << p.is_live;
+struct Point {
+	int x;
+	int y;
+
+	Point(int x, int y)
+		: x(x)
+		, y(y)
+	{
+	}
+};
+
+std::ostream& operator<< (std::ostream& os, const Cell& c) {
+  return os << c.is_live;
 }
 
-enum {
-	__WORLD_WIDTH__ = 10,
-	__WORLD_HEIGHT__ = 25
-};
+typedef vector< vector<Cell> > Matrix;
 
+namespace {
+	// Получение координат соседей точки (окрестность мура 1 порядка)
+	std::vector<Point> neighbors(const Matrix& m, int x, int y) {
+		std::vector<Point> nb;
+
+		int x0 = (x-1 >= 0) ? (x-1) : 0;
+		int y0 = (y-1 >= 0) ? (y-1) : 0;
+
+		int xN = (x+1 < m.size()) ? (x+1) : m.size()-1;
+		int yN = (y+1 < m.size()) ? (y+1) : m.size()-1;
+
+		for( int _x = x0; _x <= xN; ++_x ){
+			for( int _y = y0; _y <= yN; ++_y ){
+				if( _x == x && _y == y ){
+					continue;
+				}
+				nb.push_back(Point(_x,_y));
+			}
+		}
+		return nb;
+	}
+
+  // Вывести на экран игровое поле
+	void print_world(const Matrix& m) {
+		auto& file = cout;
+
+		// print Header
+		file << "┌─";
+		for( int i = 0; i < m[0].size(); ++i ){
+			file << "──";
+		}
+		file << "┐\n";
+//		file << "│ " << string(m[0].size()*2, ' ') << "│\n";
+
+		// print Frame
+		for( int i = 0; i < m.size(); i++ ){
+			file << "│ ";
+			for( int j = 0; j < m[i].size(); j++ ){
+				if( m[i][j].is_live ){
+					file << '*';
+				}
+				else {
+					file << ' ';
+				}
+				file << ' ';
+			}
+			file << "│\n";
+		}
+
+		// print Footer
+		file << "└─";
+		for( int i = 0; i < m[0].size(); ++i ){
+			file << "──";
+		}
+		file << "┘\n";
+	}
+
+}
+
+/*
 namespace my {
-  template <typename T, size_t SIZE>
-  struct Row {
-    enum {
-      size = SIZE
-    };
-
-    array<T,SIZE> row;
-
-    T& operator[] (const size_t index){
-      return row[index];
-    }
-    const T& operator[] (const size_t index) const {
-      return row[index];
-    }
-
-    bool operator== (const Row<T,SIZE>& r) const {
-      return row == r.row;
-    }
-  };
-  template <typename T, size_t ROW, size_t COL>
-  struct Matrix {
-    enum {
-      row = ROW,
-      col = COL
-    };
-
-    array<Row<T,COL>, ROW> matrix;
-
-    Row<T,COL>& operator[] (const size_t index) {
-      return matrix[index];
-    }
-    const Row<T,COL>& operator[] (const size_t index) const {
-      return matrix[index];
-    }
-
-    bool operator== (const Matrix<T,ROW,COL>& m) const {
-      return matrix == m.matrix;
-    }
-    // Получение координат соседей точки (окрестность мура 1 порядка)
-    std::vector< std::pair<int,int> > neighbors(int x, int y) const {
-      std::vector< std::pair<int,int> > nb;
-
-      for( int i = x - 1; i <= x + 1; i++ ){
-        for( int j = y - 1; j <= y + 1; j++ ){
-          if( i == x && j == y ){
-            continue;
-          }
-          if( i < 0 || j < 0 ){
-            continue;
-          }
-          if( i >= ROW || j >= COL ){
-            continue;
-          }
-
-          std::pair<int,int> p;
-          p.first = i;
-          p.second = j;
-
-          nb.push_back(p);
-        }
-      }
-      return nb;
-    }
-  };
 
   template <typename T, size_t SIZE>
   void print(const Row<T,SIZE>& r) {
@@ -129,7 +135,7 @@ namespace my {
   }
 
   template <size_t ROW, size_t COL>
-  void init_world(Matrix<point,ROW,COL>& w) {
+  void init_world(Matrix<Cell,ROW,COL>& w) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, 1);
@@ -143,7 +149,7 @@ namespace my {
 
   // Количество живых клеток на игровом поле
   template <size_t ROW, size_t COL>
-  int get_live_count(const Matrix<point,ROW,COL>& w) {
+  int get_live_count(const Matrix<Cell,ROW,COL>& w) {
     int count = 0;
     for( size_t i = 0; i < w.row; i++ ){
       for( size_t j = 0; j < w.col; j++ ){
@@ -157,7 +163,7 @@ namespace my {
 
   // Количество живых соседей у клетки с координатами x, y
   template <size_t ROW, size_t COL>
-  unsigned int count_live_neighbors(const Matrix<point,ROW,COL>& w, int x, int y) {
+  unsigned int count_live_neighbors(const Matrix<Cell,ROW,COL>& w, int x, int y) {
     int count = 0;
     auto nb = w.neighbors(x, y);
 
@@ -175,12 +181,12 @@ namespace my {
 
   // Сгенерировать следующее поколение игрового мира
   template <size_t ROW, size_t COL>
-  Matrix<point,ROW,COL> next_generation(const Matrix<point,ROW,COL>& prev_world) {
-    Matrix<point,ROW,COL> w;
+  Matrix<Cell,ROW,COL> next_generation(const Matrix<Cell,ROW,COL>& prev_world) {
+    Matrix<Cell,ROW,COL> w;
 
     for( auto i = 0; i < prev_world.row; i++ ){
       for( auto j = 0; j < prev_world.col; j++ ){
-        const point& p = prev_world[i][j];
+        const Cell& p = prev_world[i][j];
         const int live_nb = count_live_neighbors(prev_world, i, j);
 
         if( !p.is_live ){
@@ -199,68 +205,70 @@ namespace my {
     return w;
   }
 
-  // Вывести на экран игровое поле
-  template <size_t ROW, size_t COL>
-  void print_world(const Matrix<point,ROW,COL>& w) {
-    //ofstream file("cppstudio.txt", ios_base::trunc);
-    //ofstream file("cppstudio.txt", ios_base::app);
-    auto& file = cout;
-		file << "┌─";
-		for( int i = 0; i < w.col; ++i ){
-			file << "──";
-		}
-		file << "─┐\n";
-		for( int i = 0; i < w.row; i++ ){
-			file << "│ ";
-			for( int j = 0; j < w.col; j++ ){
-				if( w[i][j].is_live ){
-					file << '*';
-				}
-				else {
-					file << ' ';
-				}
-				file << ' ';
-			}
-			file << " │\n";
-		}
-		file << "└─";
-		for( int i = 0; i < w.col; ++i ){
-			file << "──";
-		}
-		file << "─┘\n";
-  }
 } // namespace
+*/
 
 
 int main() {
-  my::Matrix<point,__WORLD_WIDTH__,__WORLD_HEIGHT__> world;
-  my::Matrix<point, world.row, world.col> prev_world;
+	Matrix m;
+	m.resize(11);
+	for( auto& r : m ){
+		r.resize(11);
+	}
 
-  my::init_world(world);
-//  world[2][2].is_live = true;
-//  world[3][2].is_live = true;
-//  world[4][2].is_live = true;
+	auto ng = neighbors(m, 0, 0);
+	{
+		auto t = neighbors(m, 0, m[0].size()-1);
+		ng.insert(ng.end(), t.begin(),t.end());
+	}
+	{
+		auto t = neighbors(m, m.size()-1, 0);
+		ng.insert(ng.end(), t.begin(),t.end());
+	}
+	{
+		auto t = neighbors(m, m.size()-1, m[0].size()-1);
+		ng.insert(ng.end(), t.begin(),t.end());
+	}
+	{
+		auto t = neighbors(m, (m.size()-1)/2, (m[0].size()-1)/2);
+		ng.insert(ng.end(), t.begin(),t.end());
+	}
 
-	int live_points = -1;
-	bool is_optimal = false;
+	for( auto& n : ng ){
+		m[n.x][n.y].is_live = true;
+	}
 
-	do {
-		print_world(world);
-		prev_world = world;
-    world = my::next_generation(prev_world);
+	print_world(m);
+//  my::Matrix<Cell,__WORLD_WIDTH__,__WORLD_HEIGHT__> world;
+//  my::Matrix<Cell, world.row, world.col> prev_world;
+//
+//  my::init_world(world);
+////  world[2][2].is_live = true;
+////  world[3][2].is_live = true;
+////  world[4][2].is_live = true;
+//
+//	int live_points = -1;
+//	bool is_optimal = false;
+//
+//	do {
+//		print_world(world);
+//		prev_world = world;
+//    world = my::next_generation(prev_world);
+//
+//		is_optimal = world == prev_world;
+//		live_points = my::get_live_count(world);
+//
+//		if (is_optimal) {
+//			cout << "Optimal configuration detected" << endl;
+//		}
+//
+//		if (live_points == 0) {
+//			cout << "All points died" << endl;
+//		}
+//		boost::this_thread::sleep(boost::posix_time::milliseconds(1));
+//	} while (live_points != 0 && !is_optimal);
 
-		is_optimal = world == prev_world;
-		live_points = my::get_live_count(world);
-
-		if (is_optimal) {
-			cout << "Optimal configuration detected" << endl;
-		}
-
-		if (live_points == 0) {
-			cout << "All points died" << endl;
-		}
-		boost::this_thread::sleep(boost::posix_time::milliseconds(1));
-	} while (live_points != 0 && !is_optimal);
+	cout << "DONE" << endl;
 
   return 0;
 }
